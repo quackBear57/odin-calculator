e839c06a2779059531e5cca5c2607817677afaef
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-10 19:00:00 -0600

    code edits to calculator.js

--- calculator.js
+++ calculator.js
@@ -1,20 +1,43 @@
+const content = document.querySelector("#content");
+
 function loadCalculator() {
-    // make buttons & assing functions
+    // make buttons & assign functions
+
+    for (let i = 0; i < 5; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.style.display = "flex";
+        content.appendChild(row);
+        
+        for (let j = 0; j < 4; j++) {
+            let tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.style.minHeight = '60px';
+            tile.style.minWidth = '60px';
+            row.appendChild(tile);
+
+            //add buttons
+            let button = document.createElement('btn');
+            button.textContent = i + 1 + j * 3;
+            tile.appendChild(button);
+        }
+    }
 }
 
-function add() {
-
+function add(x, y) {
+    return x + y;
 }
 
-function subtract() {
-
+function subtract(x, y) {
+    return x - y;
 }
 
-function multiply() {
-
+function multiply(x, y) {
+    return x * y;
 }
 
-function divide() {
-
+function divide(x, y) {
+    return x / y;
 }
 
+loadCalculator();
